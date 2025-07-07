f8d88acfd83585963faeed4347a87f29408c67e3
BlackS717 <137413767+BlackS717@users.noreply.github.com>
2025-07-07 10:04:59 +0300

    footer layout

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,7 @@
 <div class="row">
-    <div class="col-lg-9 mx-auto">
-        <div class="row d-flex justify-content-between p-2">
-            <div class="col-lg-4 d-none d-lg-block">
+    <div class="col-12 mx-auto">
+        <div class="row p-3">
+            <div class="col-sm-6">
                 <p class="fw-bold figure-caption text-black">About</p>
                 <p class="figure-caption text-black">
                     We are a forward-thinking organization committed to innovation, collaboration, and global impact. With a growing community of dedicated professionals, we strive to create meaningful change and shape a better future for all.
@@ -15,24 +15,26 @@
                 <img src="../assets/images/fan.svg" alt="" class="img-fluid border rounded-1 p-2">
                 <img src="../assets/images/linkedin.svg" alt="" class="img-fluid border rounded-1 p-2">
             </div>
-            <div class="col-lg-4">
-                <p class="fw-bold figure-caption text-black">Navigations</p>
-                <div class="row d-flex justify-content-between">
-                    <div class="d-flex flex-column justify-content-between col-lg-4 col-5">
+            <div class="col-sm-6">
+                <p class="fw-bold figure-caption text-black row">Navigations</p>
+                <div class="row">
+                    <div class="d-flex flex-column col-4">
                         <div class="figure-caption text-black">Home</div>
                         <div class="figure-caption text-black">Services</div>
                         <div class="figure-caption text-black">Work</div>
                         <div class="figure-caption text-black">Process</div>
                         <div class="figure-caption text-black">About Us</div>
                     </div>
-                    <div class="d-flex flex-column justify-content-between col-lg-4 col-5">
+
+                    <div class="d-flex flex-column col-4">
                         <div class="figure-caption text-black">Press</div>
                         <div class="figure-caption text-black">Blog</div>
                         <div class="figure-caption text-black">Contact</div>
                         <div class="figure-caption text-black">Support</div>
                         <div class="figure-caption text-black">Privacy</div>
                     </div>
-                    <div class="d-flex flex-column justify-content-between col-lg-4 col-5">
+                    
+                    <div class="d-flex flex-column col-4">
                         <div class="figure-caption text-black">Privacy</div>
                         <div class="figure-caption text-black">FAQ</div>
                         <div class="figure-caption text-black">Careers</div>
--- inc/header.php
+++ inc/header.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-expand-lg bg-white">
-    <section class="container-fluid">
+    <div class="container-fluid">
         <a class="navbar-brand" href="#"><img src="../assets/images/house-fill.svg" style="width: 25px;" alt=""></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <section class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item ">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item">
                     <a class="nav-link" href="../pages/index.php" aria-expanded="true">Home</a>
                 </li>
                 <li class="nav-item">
@@ -16,6 +16,6 @@
                     <a class="nav-link" href="../pages/recherche.php">Search</a>
                 </li>
             </ul>
-        </section>
-    </section>
+        </div>
+    </div>
 </nav>
